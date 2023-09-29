73dc44a216689cbe748dde891ad6c39b50892f65
ItalodeCastro <69430673+ItalodeCastro@users.noreply.github.com>
2023-09-29 08:19:03 +0200

    Atualizado sem error

---
+++ src/main/java/com/campusdual/appmazing/Controller/ProductController.java
@@ -0,0 +1,48 @@
+package com.campusdual.appmazing.Controller;
+
+import com.campusdual.appmazing.api.IProductService;
+import com.campusdual.appmazing.model.dto.ProductDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/products")
+public class ProductController {
+
+    @Autowired
+    IProductService productService;
+
+    @GetMapping
+    public String testController() {
+        return "Product controller works!";
+    }
+
+    @PostMapping
+    public String testController(@RequestBody String name) {
+        return "Product controller works, " + name + "!";
+    }
+
+    @GetMapping(value = "/testMethod")
+    public String testControllerMethod(){
+        return "Product controller method works!";
+    }
+
+    @PostMapping(value = "/get")
+    public ProductDTO queryProduct(@RequestBody ProductDTO product){
+        return this.productService.queryProduct(product);
+    }
+    @GetMapping (value = "/getAll")
+    public List<ProductDTO> queryAllProducts () {
+        return this.productService.queryAllProducts();
+    }
+
+    @PostMapping (value = "/add")
+    public int insertProduct (@RequestBody ProductDTO product){
+        return this.productService.insertProduct(product);
+
+    }
+
+        //"date_added": "2022-01-01T23:00:00.000+00:00"
+}
---
+++ src/main/java/com/campusdual/appmazing/api/IProductService.java
@@ -0,0 +1,15 @@
+package com.campusdual.appmazing.api;
+
+import com.campusdual.appmazing.model.dto.ProductDTO;
+
+import java.util.List;
+
+public interface IProductService {
+    ProductDTO queryProduct (ProductDTO productDTO);
+    List<ProductDTO > queryAllProducts ();
+
+    int insertProduct (ProductDTO product);
+    int updateProduct (ProductDTO product);
+    int deleteProduct (ProductDTO product);
+
+}
